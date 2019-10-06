3e3e8577977cb920c5e0620d1b6c7930cc280e70
NAMAN <bansalnaman15@gmail.com>
2019-10-06 19:15:28 +0530

    first commit

---
+++ server.js
@@ -0,0 +1,54 @@
+var http = require("http")
+var url = require("url")
+var fs = require("fs")
+objfile = fs.readFileSync("data.json")
+objlist = JSON.parse(objfile)
+var Oproduct = fs.readFileSync("product.html")
+var HOMEPAGE = fs.readFileSync("overview.html")
+var cardfile = fs.readFileSync("card.html")
+
+HOMEPAGE = HOMEPAGE + "";
+Oproduct = Oproduct + "";
+cardfile = cardfile + "";
+
+function replace(id, product) {
+    product = product.replace(/{%PRODUCT NAME%}/g, objlist[id]["productName"])   
+    product = product.replace(/{%Description%}/g, objlist[id]["description"])
+    product = product.replace(/{%from%}/g, objlist[id]["from"])
+    product = product.replace(/{%price%}/g, objlist[id]["price"])
+    product = product.replace(/{%id%}/g, id)
+    product = product.replace(/{%nutrients%}/g, objlist[id]["nutrients"])
+    product = product.replace(/{"%PRODUCTquantity"}/g, objlist[id]["quantity"])
+    product = product.replace(/{"%PRODUCT iMAGE"}/g, objlist[id]["image"])
+    if (objlist[id]["organic"] == true)
+        product = product.replace(/{%Organic%}/g, "Organic")
+    else product = product.replace(/{%Organic%}/g, "Inorganic")
+    return product
+}
+var server = http.createServer(function (req, res) {
+    var parseurl = url.parse(req.url, true)
+    if (req.url == '/' || req.url == "") {
+        var cards=""
+        for (var i = 0; i < objlist.length; i++){
+            card = replace(i, cardfile)
+            cards = cards + card
+        }
+        HOMEPAGE = HOMEPAGE.replace(/{%cardsarea%}/g, cards)
+        res.write(HOMEPAGE)
+    }
+    else if (parseurl["pathname"] == "/product") {
+        id = parseurl["query"]["id"]
+        console.log(id)
+        product=Oproduct
+        product = replace(id,product)
+        res.write(product)
+    }
+    else if (req.url == "/api") {
+      res.write(objfile)
+    } 
+    res.end();
+})
+
+server.listen(1502, function () {
+    console.log("Server running on ")
+})
